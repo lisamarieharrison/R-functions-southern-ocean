5504b63eac90abc61933ff6683d7d61c241f61e3
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2015-11-25 11:09:03 +1100

    Wasn't working because function wasn't returned. Changed to nested functions

--- rocCurve.R
+++ rocCurve.R
@@ -2,23 +2,32 @@
 #author: Lisa-Marie Harrison
 #date: 19/11/2015
 
-rocCurve <- function(model, s, data, print = FALSE) {
+
+rocCurve <- function(model, threshold, data, print = FALSE) {
   
-  #model= binomial glm model from glmer
-  #s = threshold (usually 0.5)
-  #data = data set with column named 'pa' for presence absence data
-  #print= boolean for printing table of observed vs predicted
-  
-  Y <- na.omit(data)
-  S <- predict(model, type = "response")
-  Ps <- (S > s)*1
-  FP <- sum((Ps == 1)*(Y == 0))/sum(Y == 0)
-  TP <- sum((Ps == 1)*(Y == 1))/sum(Y == 1)
-  if (print) {
-    print(table(Observed = Y, Predicted = Ps))
+  roc.curve <- function(s, print = FALSE){
+    
+    Y  <- na.omit(data)
+    S  <- predict(model, type = "response")
+    Ps <- (S > s)*1
+    FP <- sum((Ps == 1)*(Y == 0))/sum(Y == 0)
+    TP <- sum((Ps == 1)*(Y == 1))/sum(Y == 1)
+    
+    vect <- c(FP,TP)
+    names(vect) <- c("FPR", "TPR")
+    
+    if(print){
+      print(table(Observed = Y,Predicted = Ps))
+      print(vect)
+    }
+    return(vect)
   }
-  vect <- c(FP, TP)
-  names(vect) <- c("FPR", "TPR")
-  return(vect)
+  
+  roc.curve(threshold, print)
+  ROC.curve <- Vectorize(roc.curve)
+  M.ROC     <- ROC.curve(seq(0, 1, by = 0.01))
+  
+  return(M.ROC)
+  
 }
 
