7a7547004454150b659a4d209e1d22dd12aa606e
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-03-08 15:36:31 +1100

    Fixed to include models with Intercept

--- getFittedGWR.R
+++ getFittedGWR.R
@@ -6,14 +6,26 @@ getFittedGWR <- function(model, dat) {
   
   #model: a gwr model object from gwr.generalised 
   #dat: data.frame of data with same column names as coefficients in model
-
+  
   coefs <- names(model$glm.res$coefficients)
   
-  est <- as.data.frame(model$SDF)[, na.omit(match(names(model$SDF), coefs))]
-  
-  df <- dat[, match(coefs, names(dat))]
-  
-  model.fitted <- exp(rowSums(est * df))
+  if (any(coefs == "Intercept")) {
+    
+    coefs <- coefs[!coefs == "Intercept"]
+    
+    est <- as.data.frame(model$SDF)[, match(coefs, names(model$SDF))]
+    df <- dat[, na.omit(match(coefs, names(dat)))]
+    
+    model.fitted <- exp(rowSums(est * df) + model$SDF$Intercept)   
+    
+  } else {
+    
+    est <- as.data.frame(model$SDF)[, na.omit(match(names(model$SDF), coefs))]
+    
+    df <- dat[, match(coefs, names(dat))]
+    model.fitted <- exp(rowSums(est * df))
+    
+  }
   
   return (model.fitted)
   
