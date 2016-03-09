044031bee13ae86570584290fea04443e8de0b8f
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-03-09 15:35:25 +1100

    Added functionality for calculating distances to multiple points

--- gcdHF.R
+++ gcdHF.R
@@ -9,7 +9,18 @@ gcdHF <- function(lat1, long1, lat2, long2) {
   delta.long <- (long2 - long1)
   delta.lat <- (lat2 - lat1)
   a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) * sin(delta.long/2)^2
-  c <- 2 * asin(min(1,sqrt(a)))
+  
+  if (length(lat2) == 1) {
+    
+    c <- 2 * asin(min(1,sqrt(a)))
+    
+  } else {
+    
+    a[a > 1] <- 1
+    c <- 2 * asin(sqrt(a))
+    
+  }
+  
   d = R * c
   
   return(d) # Distance in km
