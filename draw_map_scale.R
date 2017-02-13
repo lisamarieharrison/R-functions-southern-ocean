417612087fd1d40cc2c15086a10b1a3533a01a09
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2017-02-13 14:10:54 +1100

    Not sure of changes

--- draw_map_scale.R
+++ draw_map_scale.R
@@ -60,10 +60,10 @@ createOrientationArrow <- function(scaleBar, length, distance = 1, dist.units = 
 scaleBar <- function(lon, lat, distanceLon, distanceLat, distanceLegend, dist.unit = "km", rec.fill = "white", rec.colour = "black", rec2.fill = "black", rec2.colour = "black", legend.colour = "black", legend.size = 3, orientation = TRUE, arrow.length = 500, arrow.distance = 300, arrow.North.size = 6){
   laScaleBar <- createScaleBar(lon = lon, lat = lat, distanceLon = distanceLon, distanceLat = distanceLat, distanceLegend = distanceLegend, dist.unit = dist.unit)
   # First rectangle
-  rectangle1 <- geom_polygon(data = laScaleBar$rectangle, aes(x = lon, y = lat), fill = rec.fill, colour = rec.colour)
+  rectangle1 <- geom_polygon(data = laScaleBar$rectangle, aes(x = lon, y = lat, group = 1), fill = rec.fill, colour = rec.colour)
   
   # Second rectangle
-  rectangle2 <- geom_polygon(data = laScaleBar$rectangle2, aes(x = lon, y = lat), fill = rec2.fill, colour = rec2.colour)
+  rectangle2 <- geom_polygon(data = laScaleBar$rectangle2, aes(x = lon, y = lat, group = 1), fill = rec2.fill, colour = rec2.colour)
   
   # Legend
   scaleBarLegend <- annotate("text", label = paste(laScaleBar$legend[,"text"], dist.unit, sep=""), x = laScaleBar$legend[,"long"], y = laScaleBar$legend[,"lat"], size = legend.size, colour = legend.colour)
@@ -72,7 +72,7 @@ scaleBar <- function(lon, lat, distanceLon, distanceLat, distanceLegend, dist.un
   
   if(orientation){# Add an arrow pointing North
     coordsArrow <- createOrientationArrow(scaleBar = laScaleBar, length = arrow.length, distance = arrow.distance, dist.unit = dist.unit)
-    arrow <- list(geom_segment(data = coordsArrow$res, aes(x = x, y = y, xend = xend, yend = yend)), annotate("text", label = "N", x = coordsArrow$coordsN[1,"x"], y = coordsArrow$coordsN[1,"y"], size = arrow.North.size, colour = "black"))
+    arrow <- list(geom_segment(data = coordsArrow$res, aes(x = x, y = y, xend = xend, yend = yend, group = 1)), annotate("text", label = "N", x = coordsArrow$coordsN[1,"x"], y = coordsArrow$coordsN[1,"y"], size = arrow.North.size, colour = "black"))
     res <- c(res, arrow)
   }
   return(res)
